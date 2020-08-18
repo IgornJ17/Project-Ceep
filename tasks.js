7f241a0ce870ef29be49c34127430933314ab03c
HASH <igorjuliani@hotmail.com>
2020-08-18 15:34:34 -0300

    Aula 2

--- tasks.js
+++ tasks.js
@@ -4,5 +4,7 @@ const buttonSelect = document.querySelector("[data-form-button]");
 
 buttonSelect.addEventListener("click", function(event){
     event.preventDefault();
-    console.log("fui clicado");
+    var inputSelect = document.querySelector("[data-form-input]");
+    var getValue = inputSelect.value
+    console.log("Valor informado pelo Usuario: ", getValue);
 })
