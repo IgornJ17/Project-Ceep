950d23f8d3eabc1618b479a23cbc8f0993af7245
HASH <igorjuliani@hotmail.com>
2020-08-19 15:33:20 -0300

    Aula 3

--- tasks.js
+++ tasks.js
@@ -1,11 +1,22 @@
 
-const getItem = function(event){
+const createItem = function(event){
     event.preventDefault();
     var inputSelect = document.querySelector("[data-form-input]");
     var getValue = inputSelect.value
     console.log("Valor informado pelo Usuario: ", getValue);   
+
+    const selectList = document.querySelector("[data-list]");
+    const liTask = document.createElement("li");
+    liTask.classList.add("task")
+    const item = `<p class="content">${getValue}</p>`
+
+
+    liTask.innerHTML = item;
+    selectList.appendChild(liTask);
+
+    inputSelect.value = " ";
 }
 
 const buttonSelect = document.querySelector("[data-form-button]");
 
-buttonSelect.addEventListener("click", getItem);
+buttonSelect.addEventListener("click", createItem);
