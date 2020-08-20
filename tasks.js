11eb682818d27af5cc58b67a9bf14c81656d3299
HASH <igorjuliani@hotmail.com>
2020-08-20 14:52:27 -0300

    Aula 4 - Criando tarefa e executando IIFE

--- tasks.js
+++ tasks.js
@@ -1,41 +1,54 @@
 
-const CreateButton = function(){
+function main(){
+    const CreateButton = function(){
 
     var button = document.createElement('button');
     button.classList.add("check-button");
     button.textContent = "Confirmar";
 
-    button.addEventListener("click", (event) =>{
-        console.log("Fui clicado");
-    })
+    button.addEventListener("click", confirmTask);
 
     return button;
+    }
+
+    const confirmTask = (event) => {
+    var buttonSelect = event.target;
+    var selectNodeFather = buttonSelect.parentElement;
+
+    selectNodeFather.classList.toggle('done');
+    buttonSelect.textContent = "Confirmado";
+
+    }
+
+    const createItem = function(event){
+    const buttonSelect = document.querySelector("[data-form-button]");
+
+    buttonSelect.addEventListener("click", (event) => {
+        event.preventDefault();
+        var inputSelect = document.querySelector("[data-form-input]");
+        var getValue = inputSelect.value
+        console.log("Valor informado pelo Usuario: ", getValue);   
+
+        const selectList = document.querySelector("[data-list]");
+        const liTask = document.createElement("li");
+        liTask.classList.add("task")
+        const item = `<p class="content">${getValue}</p>`
+
+
+        liTask.innerHTML = item;
+        liTask.appendChild(CreateButton());
+        selectList.appendChild(liTask);
+        
+
+        inputSelect.value = " ";
+    })
+    }
+    createItem();
 }
 
-
-const createItem = function(event){
-    event.preventDefault();
-    var inputSelect = document.querySelector("[data-form-input]");
-    var getValue = inputSelect.value
-    console.log("Valor informado pelo Usuario: ", getValue);   
-
-    const selectList = document.querySelector("[data-list]");
-    const liTask = document.createElement("li");
-    liTask.classList.add("task")
-    const item = `<p class="content">${getValue}</p>`
-
-
-    liTask.innerHTML = item;
-    liTask.appendChild(CreateButton());
-    selectList.appendChild(liTask);
-    
-
-    inputSelect.value = " ";
-}
+main();
 
 
 
 
-const buttonSelect = document.querySelector("[data-form-button]");
 
-buttonSelect.addEventListener("click", createItem);
